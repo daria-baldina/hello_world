6dcebfdb55c6c6d265f82a34e88eaa9db93165df
Daria <mercayshi.svet@gmail.com>
2022-09-19 23:40:58 +0300

    ну теперь точно получилось

--- example_003_Sum/Program.cs
+++ example_003_Sum/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.Write("Введите два слагаемых через пробел");
+Console.WriteLine("Введите два слагаемых через пробел");
 string[] num = Console.ReadLine().Split(' ');
                int a = int.Parse(num[0]);
                int b= int.Parse(num[1]);
