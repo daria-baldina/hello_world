15c1b949e3e1afd45169fd94f3c3a48d92ec60f1
Daria <mercayshi.svet@gmail.com>
2022-09-20 00:03:11 +0300

    получилось

---
+++ example_004_div/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите делимое и делитель через пробел");
+string[] num = Console.ReadLine().Split(' ');
+               double a = double.Parse(num[0]);
+               double b = double.Parse(num[1]);
+Console.WriteLine(a/b);
