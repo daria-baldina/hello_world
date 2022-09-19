719ec7649020ce2b79361426392b82e038e065a4
Daria <mercayshi.svet@gmail.com>
2022-09-20 01:05:09 +0300

    initial commit

---
+++ example_005_if_else/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя ");
+string username = Console.ReadLine();
+if(username == "Маша")
+{
+    Console.WriteLine("Машенька, солнышко, привет!");
+}
+else
+{
+    Console.Write("Добрый день, ");
+    Console.WriteLine(username);
+}
