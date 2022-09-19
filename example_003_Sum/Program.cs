a899ba7b843e3f7803b4469b7a19930546201207
Daria <mercayshi.svet@gmail.com>
2022-09-19 23:38:57 +0300

    вроде наконец получилось как я хотела

--- example_003_Sum/Program.cs
+++ example_003_Sum/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.Write("Введите первое слагаемое");
-int numberA = Console.Read();
-Console.Write("Введите второе слагаемое");
-int numberB = Console.Read();
-Console.WriteLine(numberA + numberB);
+Console.Write("Введите два слагаемых через пробел");
+string[] num = Console.ReadLine().Split(' ');
+               int a = int.Parse(num[0]);
+               int b= int.Parse(num[1]);
+Console.WriteLine(a + b);
