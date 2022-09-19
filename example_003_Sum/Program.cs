ef9a9096ce427dbf1b6fc19b11747588f073ca45
Daria <mercayshi.svet@gmail.com>
2022-09-19 23:31:40 +0300

    initial commit

---
+++ example_003_Sum/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите первое слагаемое");
+int numberA = Console.Read();
+Console.Write("Введите второе слагаемое");
+int numberB = Console.Read();
+Console.WriteLine(numberA + numberB);
